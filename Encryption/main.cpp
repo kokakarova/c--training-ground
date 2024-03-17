13c82b114ba769656f1bec203f0744c55181653a
kokakarova <kostadinkakarova@gmail.com>
2024-03-17 20:07:53 +0100

    remove commented out liens

--- Encryption/main.cpp
+++ Encryption/main.cpp
@@ -1,5 +1,5 @@
 // Section 10
-// Challenge - 
+// Challenge -
 // Substitution Cipher
 
 /*
@@ -14,7 +14,7 @@ Encrypt this message using the substitution cipher and display the encrypted mes
 Then decryped the encrypted message back to the original message.
 
 You may use the 2 strings below for  your subsitition.
-For example, to encrypt you can replace the character at position n in alphabet 
+For example, to encrypt you can replace the character at position n in alphabet
 with the character at position n in key.
 
 To decrypt you can replace the character at position n in key
@@ -36,60 +36,17 @@ Reuse existing functionality in libraries and in the std::string class!
 using namespace std;
 
 int main() {
-    
+
     string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     string key  {"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
-   
 
-//  USING FOR LOOP
-//    string decrypted;
-//    string encrypted;
-//    char c{};    
-//    size_t position{};
-//    
-//    cout << "\nEnter your secret message: ";
-//    getline(cin, decrypted);
-//    
-//    cout << "\nEncrypting message ..." << endl;
-//    
-//    for (size_t i{0}; i < decrypted.length(); ++i)  {
-//        c = decrypted.at(i);
-//        position = alphabet.find(c);
-//        if (position != string::npos) 
-//            c = key.at(position);
-//        encrypted.insert(i,1,c);
-//    } 
-//    
-//    cout << "\nEncrypted message: ";
-//    cout << encrypted << endl;
-//    
-//    cout << "\nDecrypting message ..." << endl;
-//    
-//    decrypted.clear();
-//    
-//    for (size_t i{0}; i < encrypted.length(); ++i)  {
-////  FIND THE POSITION OF THE CHAR IN THE ALPHABET
-//        c = encrypted.at(i);
-//        position = key.find(c);
-////  FIND THE CHAR THAT IS AT THAT SAME POSITION IN THE KEY STRING
-//        if (position != string::npos) {
-//            c = alphabet.at(position);
-//        }
-//        decrypted.insert(i,1,c);
-//    }
-//    
-//    cout << "\nDecrypted message: ";
-//    cout << decrypted << endl;
-    
-//  USING RANGE BASED FOR LOOP    
-    
     string secret_message{};
     cout << "\nEnter your secret message: ";
     getline(cin, secret_message);
-    
+
     cout << "\nEncrypting message ..." << endl;
     string encrypted_message{};
-    
+
     for (char c : secret_message)   {
         size_t position = alphabet.find(c);
         if (position != string::npos)   {
@@ -98,14 +55,14 @@ int main() {
             encrypted_message += c;
         }
     }
-    
+
     cout << "\nEncrypted message: ";
     cout << encrypted_message << endl;
-    
+
     cout << "\nDecrypting message ..." << endl;
-    
+
     string decrypted_message{};
-    
+
     for (char c : encrypted_message)   {
         size_t position = key.find(c);
         if (position != string::npos)   {
@@ -116,9 +73,8 @@ int main() {
     }
     cout << "\nDecrypted message: ";
     cout << decrypted_message << endl;
-    
-    
+
+
     cout << endl;
     return 0;
 }
-
